d1192eb817e0f5e9bc2eda166d80693d66867ec3
Balya Elfata <balyaelfata@yahoo.com>
2025-02-25 15:14:50 +0700

    feat: Add context to VC

--- CoreDataExample/ViewController.swift
+++ CoreDataExample/ViewController.swift
@@ -8,6 +8,9 @@
 import UIKit
 
 class ViewController: UIViewController {
+
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
