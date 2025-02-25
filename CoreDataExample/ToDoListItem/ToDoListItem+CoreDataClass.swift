b1c66d2365095a730da57cb165c032e910754a49
Balya Elfata <balyaelfata@yahoo.com>
2025-02-25 15:04:57 +0700

    Initial Commit

---
+++ CoreDataExample/ToDoListItem/ToDoListItem+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoListItem+CoreDataClass.swift
+//  CoreDataExample
+//
+//  Created by Balya Elfata on 25/02/25.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ToDoListItem)
+public class ToDoListItem: NSManagedObject {
+
+}
---
+++ CoreDataExample/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  CoreDataExample
+//
+//  Created by Balya Elfata on 25/02/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
