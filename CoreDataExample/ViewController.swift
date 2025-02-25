c55ad17c666798c3d4a5d29108a532995e9127d2
Balya Elfata <balyaelfata@yahoo.com>
2025-02-25 20:47:14 +0700

    feat: implement edit and delete alert upon clicking item

--- CoreDataExample/ViewController.swift
+++ CoreDataExample/ViewController.swift
@@ -9,7 +9,6 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     private var models = [ToDoListItem]()
-
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
     let tableView: UITableView = {
@@ -59,6 +58,41 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
         cell.textLabel?.text = model.name
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        let item = models[indexPath.row]
+        
+        let sheet = UIAlertController(title: "Edit",
+                                      message: nil,
+                                      preferredStyle: .actionSheet)
+        
+        sheet.addAction(UIAlertAction(title: "Cancel", style: .cancel) {_ in })
+        sheet.addAction(UIAlertAction(title: "Edit", style: .default) { _ in
+            let alert = UIAlertController(title: "New Item",
+                                          message: "Enter New Item",
+                                          preferredStyle: .alert)
+            
+            alert.addTextField(configurationHandler: nil)
+            alert.textFields?.first?.text = item.name
+            alert.addAction(UIAlertAction(title: "Save", style: .cancel) { [weak self] _ in
+                guard let field = alert.textFields?.first, let newName = field.text, !newName.isEmpty else {
+                    return
+                }
+                
+                self?.updateItem(item: item, newName: newName)
+                self?.getAllItems()
+            })
+            
+            self.present(alert, animated: true)
+        })
+        sheet.addAction(UIAlertAction(title: "Submit", style: .destructive) { [weak self] _ in
+            self?.deleteItem(item: item)
+        })
+        
+        present(sheet, animated: true)
     }
     
     func getAllItems() {
