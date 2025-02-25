b18fd841152d40e1d8988fcf949dff9d9ca272c9
Balya Elfata <balyaelfata@yahoo.com>
2025-02-25 15:15:12 +0700

    feat: Add CRUD functions to VC

--- CoreDataExample/ViewController.swift
+++ CoreDataExample/ViewController.swift
@@ -15,5 +15,49 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    func getAllItems() {
+        do {
+            let items = try context.fetch(ToDoListItem.fetchRequest())
+        }
+        catch {
+            // error
+        }
+    }
+    
+    func createItem(name: String) {
+        let newItem = ToDoListItem(context: context)
+        newItem.name = name
+        newItem.createdAt = Date()
+        
+        do {
+            try context.save()
+        }
+        catch {
+            // error
+        }
+    }
+    
+    func deleteItem(item: ToDoListItem) {
+        context.delete(item)
+        
+        do {
+            try context.save()
+        }
+        catch {
+            // error
+        }
+    }
+    
+    func updateItem(item: ToDoListItem, newName: String) {
+        item.name = newName
+        
+        do {
+            try context.save()
+        }
+        catch {
+            // error
+        }
+    }
 }
 
